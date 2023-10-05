7b9f891539fe393aeef6185d3bdf8e41d8239512
Abdul Quddos <abdulquddoshafiz@gmail.com>
2023-10-06 00:13:48 +0500

    starting done

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Index.module.css'
+const index = () => {
+  return (
+    <div className={styles.mainContainer}>
+       <h1>Hello This is me abdul quddos here</h1>
+    </div>
+  )
+}
+export default index
