d46c5500cf43267a1a9eceb4559d733a2cd6ca2c
Abdul Quddos <abdulquddoshafiz@gmail.com>
2023-10-06 18:44:05 +0500

    Starting the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './components/Home/Index'
 function App() {
   return (
     <div className="App">
+    <Home/>
     </div>
   );
 }
--- src/components/Home/Navbar/Index.jsx
+++ src/components/Home/Navbar/Index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from './Index.module.css'
-import Navbar from './Navbar/Index'
 const Index = () => {
   return (
     <div className={styles.mainContainer}>
-    <Navbar/>
+    
+
     </div>
   )
 }
+
 export default Index
